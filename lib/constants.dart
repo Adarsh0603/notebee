ecd3d45424e7d0a6ca7de2b02724d5803e38be7f
Adarsh Verma <adarshverma0603@gmail.com>
2020-08-11 20:08:15 +0530

    added delete icon ,date and fixed label

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 
-const kNoteTitleTextStyle = TextStyle(color: Colors.black);
+const kNoteTitleTextStyle =
+    TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18);
 
-const kNoteContentTextStyle = TextStyle(
-  color: Colors.grey,
-);
+const kNoteContentTextStyle =
+    TextStyle(color: Colors.grey, fontWeight: FontWeight.w400, fontSize: 14);
+const kNoteDateTextStyle = TextStyle(color: Colors.grey, fontSize: 12);
 
 const kAddNoteTitleTextStyle =
     TextStyle(fontWeight: FontWeight.w600, fontSize: 28, color: Colors.black);
--- lib/widgets/note_widget.dart
+++ lib/widgets/note_widget.dart
@@ -4,9 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_database_sql/constants.dart';
 import 'package:flutter_database_sql/models/note.dart';
 import 'package:flutter_database_sql/providers/notes_provider.dart';
-import 'package:flutter_database_sql/services/utils.dart';
 import 'package:flutter_database_sql/widgets/note_insert_widget.dart';
 import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
 
 class NoteWidget extends StatefulWidget {
   final Note note;
@@ -18,7 +18,7 @@ class NoteWidget extends StatefulWidget {
 }
 
 class _NoteWidgetState extends State<NoteWidget> {
-  bool open = false;
+  bool open = true;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -55,42 +55,78 @@ class _NoteWidgetState extends State<NoteWidget> {
       },
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-        child: Dismissible(
-          key: ValueKey(widget.note.id),
-          background: Container(
-            color: Colors.black,
-            child: Icon(Icons.delete),
-          ),
-          onDismissed: (_) async {
-            await Provider.of<Notes>(context, listen: false)
-                .deleteNote(widget.note.id);
-          },
-          child: Container(
-            width: double.infinity,
-            padding: EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey, width: 0.2),
-                borderRadius: BorderRadius.all(Radius.circular(8.0))),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(widget.note.title, style: kNoteTitleTextStyle),
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey, width: 0.2),
+              borderRadius: BorderRadius.all(Radius.circular(8.0))),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CircleAvatar(maxRadius: 6, backgroundColor: Colors.green),
+                    SizedBox(width: 8.0),
+                    Text(widget.note.title, style: kNoteTitleTextStyle),
+                    Spacer(),
+                    Text(
+                        DateFormat('dd.MM.yyyy')
+                            .format(DateTime.parse(widget.note.id))
+                            .toString(),
+                        style: kNoteDateTextStyle),
+                  ],
                 ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
+                child: Text(
+                  widget.note.content,
+                  softWrap: true,
+                  overflow: open ? TextOverflow.clip : TextOverflow.ellipsis,
+                  style: kNoteContentTextStyle,
+                ),
+              ),
+              if (open) SizedBox(height: 8.0),
+              if (open) Divider(height: 1),
+              if (open)
                 Padding(
-                  padding:
-                      const EdgeInsets.only(left: 8.0, right: 8.0, bottom: 8.0),
-                  child: Text(
-                    widget.note.content,
-                    softWrap: true,
-                    overflow: open ? TextOverflow.clip : TextOverflow.ellipsis,
-                    style: kNoteContentTextStyle,
+                  padding: const EdgeInsets.only(
+                      left: 8.0, right: 8.0, top: 4.0, bottom: 4.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Double Tap to edit.',
+                        style: kNoteDateTextStyle.copyWith(
+                            color: Colors.lightBlueAccent),
+                      ),
+                      Container(
+                        width: 20,
+                        height: 20,
+                        child: IconButton(
+                          padding: EdgeInsets.all(0),
+                          onPressed: () async {
+                            await Provider.of<Notes>(context, listen: false)
+                                .deleteNote(widget.note.id);
+                          },
+                          icon: Icon(
+                            Icons.delete_outline,
+//                              size: 20,
+                            color: Colors.lightBlueAccent,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
+            ],
           ),
         ),
       ),
